41eb0e93f16b449923b3316c3848e66c5634a1e4
mayankpmahajan <mayankpmahajan@gmail.com>
2025-01-03 18:53:34 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import WorkflowBuilder from "../components/workflow/WorkflowBuilder"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <WorkflowBuilder />
+    </div>
+  );
+}
